971fed8c86faf8f69691928dae880d0e111965b3
RogueRebel97 <seyamanx@hotmail.com>
2022-03-03 22:37:56 +0100

    Calculadora React Acabada

--- src/components/Calculadora.js
+++ src/components/Calculadora.js
@@ -1,108 +1,134 @@
-
 import React from "react";
 
 class Calculadora extends React.Component {
-    // ni el constructor ni las props son estrictamente necesarios
-    constructor(props) {
-        super(props)
-        this.state = {
-            num1: "",
-            num2: "",
-            resultado: ""
-        }
+  ArrayHistorial = [];
 
+  // ni el constructor ni las props son estrictamente necesarios
+  constructor(props) {
+    super(props);
+    this.state = {
+      num1: "",
+      num2: "",
+      resultado: "",
+      historial: [],
+    };
+  }
 
+  handlerInput = (evento) => {
+    console.log(evento);
+    let name = evento.target.name;
+    this.setState({
+      [name]: Number(evento.target.value),
+    });
+  };
 
+  sumar = () => {
+    this.historial(
+      `${this.state.num1} + ${this.state.num2} = ${
+        this.state.num1 + this.state.num2
+      } `
+    );
+    this.setState({
+      resultado: this.state.num1 + this.state.num2,
+    });
+  };
 
+  historial = (operacion) => {
+    this.setState({
+      historial: [...this.state.historial, operacion],
+    });
+  };
 
-    }
+  restar = () => {
+    this.historial(
+      `${this.state.num1} - ${this.state.num2} = ${
+        this.state.num1 - this.state.num2
+      } `
+    );
+    this.setState({
+      resultado: this.state.num1 - this.state.num2,
+    });
+  };
 
-    handlerInput = (evento) => {
+  mult = () => {
+    this.historial(
+      `${this.state.num1} x ${this.state.num2} = ${
+        this.state.num1 * this.state.num2
+      } `
+    );
+    this.setState({
+      resultado: this.state.num1 * this.state.num2,
+    });
+  };
 
+  div = () => {
+    this.historial(
+      `${this.state.num1} / ${this.state.num2} = ${
+        this.state.num1 / this.state.num2
+      } `
+    );
+    this.setState({
+      resultado: this.state.num1 / this.state.num2,
+    });
+  };
 
-        console.log(evento);
-        let name = evento.target.name;
-        this.setState({
-            [name]: Number(evento.target.value)
-        })
-    }
+  bin = () => {
+    this.historial(`${this.state.num1} = ${this.state.num1.toString(2)} `);
+    this.setState({
+      resultado: this.state.num1.toString(2),
+    });
+  };
 
-    sumar = () => {
-        this.setState({
-            resultado: (this.state.num1 + this.state.num2)
-        })
+  limpiar = () => {
+    this.setState({
+      historial: [],
+      resultado: "",
+      num1: "",
+      num2: "",
+    });
+  };
 
+  render() {
+    // Esto es lo que mostrara el componente
+    console.log(this.state.historial);
+    return (
+      <div class="capaExterior">
+        <h1>Calculadora</h1>
+        <p>
+          <span>
+            Primer Numero{" "}
+            <input type="number" name="num1" onChange={this.handlerInput} />{" "}
+          </span>
+          <span>
+            Segundo Numero{" "}
+            <input
+              type="text"
+              name="num2"
+              onChange={this.handlerInput}
+              disabled={this.state.num1.length < 1}
+            />{" "}
+          </span>
+          Resultado: {this.state.resultado}
+        </p>
 
-    }
+        <hr />
+        <button onClick={this.sumar}> Sumar</button>
+        <button onClick={this.restar}> Resta</button>
+        <button onClick={this.mult}> Multiplicar</button>
+        <button onClick={this.div}> Dividir</button>
+        <button onClick={this.bin}> Binario</button>
 
-    restar = () => {
-        this.setState({
-            resultado: (this.state.num1 - this.state.num2)
-        })
-
-
-    }
-
-    mult = () => {
-        this.setState({
-            resultado: (this.state.num1 * this.state.num2)
-        })
-
-
-    }
-
-    div = () => {
-        this.setState({
-            resultado: (this.state.num1 / this.state.num2)
-        })
-
-
-    }
-
-    bin = () => {
-        this.setState({
-            resultado: this.state.num1.toString(2)
-        })
-
-
-    }
-
-
-    render() {
-
-
-        // Esto es lo que mostrara el componente
-        return (
-            <div class="capaExterior">
-                <h1>Calculadora</h1>
-                <p>
-                    <span>Primer Numero <input type="number" name="num1" onChange={this.handlerInput} /> </span>
-                    <span>Segundo Numero <input type="text" name="num2" onChange={this.handlerInput} disabled={this.state.num1.length < 1} /> </span>
-                    Resultado: {this.state.resultado}
-                </p>
-
-                <hr />
-
-                <button onClick={this.sumar}> Sumar</button>
-                <button onClick={this.restar}> Resta</button>
-                <button onClick={this.mult}> Multiplicar</button>
-                <button onClick={this.div}> Dividir</button>
-                <button onClick={this.bin}> binario</button>
-
-                <hr />
-                <h2>Historial</h2>
-
-            </div>
-
-
-
-
-
-
-
-        )
-    }
-
+        <hr />
+        <h2>Historial</h2>
+        <ul>
+          {this.state.historial.map((e, i) => {
+            return <li key={i}>{e}</li>;
+          })}
+        </ul>
+        <button onClick={this.limpiar}>Limpiar</button>
+      </div>
+    );
+  }
 }
 
 export default Calculadora;
